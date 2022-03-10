cbbe3718252f004556694ec838ad8468fd604874
chengguangzhuang <1223445257@qq.com>
2022-03-10 15:52:04 +0800

    feat: init

---
+++ main.js
@@ -0,0 +1,13 @@
+const http = require('http')
+
+const port = 80
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('666\n')
+})
+
+server.listen(port, () => {
+  console.log(`服务器运行在 http://0.0.0.0:${port}/`)
+})
